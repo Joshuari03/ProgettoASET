5dac5ffe021a561b964f397df2ffeb080016867c
Joshuari <Joshuarizzello03@gmail.com>
2025-10-23 11:11:25 +0200

    aggiornati script per scalabilità aggiunti log miki

--- Script/visualization.m
+++ Script/visualization.m
@@ -1,5 +1,4 @@
-
-%% plot vari
+%% plot velocità GPS
 
 figure
 plot(time_GPS, speed*3.6);
@@ -7,28 +6,34 @@ title('velocità')
 grid on
 grid minor
 
-% figure
-% plot(time_GPS,PositionAndata.hacc);
-% title('errore posizione')
+%% plot errore GPS
+figure
+plot(time_GPS,PositionAndata.hacc);
+title('errore posizione')
 
+%% plot accelerazione grezza accelerometro
 figure
 plot(time_acc, acc);
 title('accelerazione')
 grid on
 grid minor
 
+%% plot FFT accelerazione
 figure
 plot(f_acc, abs(A));
 title('FFT accelerazione')
 
+%% plot accelerazione ricavata dalla velocità derivando
 figure
 plot(time_GPS, dv);
 title('accelerazione ricavata dalla velocità derivando')
 
+%% plot FFT accelerazione derivata
 figure
 plot(f_GPS, abs(DV));
 title('FFT accelerazione derivata') 
 
+%% plot accelerazione pulita
 figure
 plot(time_acc, a_cleaned);
 title('Accelerazione pulita')
