27ae28c82778641e901ada590b0586c8e3fbaead
Joshuari <Joshuarizzello03@gmail.com>
2025-10-24 14:07:22 +0200

    Stima più completa dell'energia. Bisogna verificarla, è approssimativa!

--- Script/Energy.m
+++ Script/Energy.m
@@ -4,6 +4,11 @@ v = speed;
 t_gps = Position.Timestamp;
 t_acc = Acceleration.Timestamp;
 t_s = seconds(t_acc - t_acc(1));
+g = 9.80665; % accelerazione di gravità
+Crr = 0.008; % coefficiente di attrito ruota strada
+rho = 1.225; % coefficiente attrito dell'aria
+CdA = 8; % area frontale bus per attrito aero
+eta = 0.8; % efficienza trasmissione tra batteria e ruote
 
 v_interp = interp1(t_gps, v, t_acc, 'linear', 'extrap');
 % figure
@@ -14,8 +19,11 @@ grid on
 grid minor
 title('velocità interpolata km/h')
 
+%% Potenza servizi ausiliari
+p_aux = 5000;
 
-p_acc = m*a.*v_interp;
+%% calcolo potenza istantanea
+p_acc = m *a .*v_interp;
 
 figure
 plot(t_acc , p_acc);
@@ -23,7 +31,11 @@ grid on
 grid minor
 title('potenza istantanea')
 
+%% calcolo potenza attrito ruota gomma
+p_roll = m * g * Crr .* v_interp;
 
+%% calcolo potenza attrito aerodinamico
+p_aero   = 0.5 * rho * CdA .* v_interp.^3;
 
 %% calcolo energia
 p_acc_no_regen = p_acc;
@@ -33,8 +45,20 @@ for k=1:1:length(p_acc)
     end
 end
 
-figure
-plot(t_acc, p_acc_no_regen)
+% figure
+% plot(t_acc, p_acc_no_regen)
 
-E_no_regen = trapz(t_s, p_acc_no_regen)/ 3.6e6;
-disp(E_no_regen);
+p_tot_raw = p_acc_no_regen + p_roll + p_aero + p_aux;
+p_battery = p_tot_raw./eta;
+
+E = trapz(t_s, p_battery)/ 3.6e6;
+% disp(E_no_regen);
+% 
+% E_roll = trapz(t_s, p_roll)/ 3.6e6;
+% 
+% E_aero = trapz(t_s, p_aero)/ 3.6e6;
+% 
+% E_aux = trapz(t_s, p_aux)/ 3.6e6;
+% 
+% E = E_no_regen + E_roll + E_aero + E_aux;
+disp(E);  % in kWh !!
