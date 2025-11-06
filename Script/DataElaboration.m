ff543df78ba6ad57fe036b50dc1fcca0bc268ab7
MicheleSavoldelli <mikysav03@gmail.com>
2025-11-06 11:17:50 +0100

    Un paio di modifiche
    Trasformato energy in funzine e fatto un paio di modifiche allo script, allargato for in data elaboration e richiamato energy lì dentro

--- Script/DataElaboration.m
+++ Script/DataElaboration.m
@@ -15,54 +15,60 @@ D =[load("Miki_973_A_1.mat");
 %=======
 %>>>>>>> Stashed changes
 %>>>>>>> Stashed changes
+ E_no_regen = NaN(size(D,1),1);
 %% data for plots
 
 for i=1:size(D,1)
-    
+
     time_acc = D(i).Acceleration.Timestamp;
     time_GPS = D(i).Position.Timestamp;
     speed = D(i).Position.speed;
     acc = D(i).Acceleration.Y;
-    acc = acc(i) - mean(acc(i));
+    acc = acc - mean(acc);
 
-end %ESTENDERE FOR E RICHIAMARE ENERGY IN QUESTO SCRIPT
-%% accelerometro
+    %RICHIAMARE ENERGY IN QUESTO SCRIPT
+    %% accelerometro
 
-A = fft(acc);
+    A = fft(acc);
 
-fs_acc = 20;
-N = length(abs(A));
-f_acc = (0:N-1)*(fs_acc/N);
+    fs_acc = 20; %deciso da noi durante la raccolta dati
+    N = length(abs(A));
+    f_acc = (0:N-1)*(fs_acc/N);
 
-if mod(length(A),2)==0
-    half_len = (length(A))/2;
-elseif mod(length(A),2)~=0
-    half_len = (length(A)+1)/2;
+    if mod(length(A),2)==0
+        half_len = (length(A))/2;
+    elseif mod(length(A),2)~=0
+        half_len = (length(A)+1)/2;
+    end
+    A_half = A(1:half_len);
+    f_cut = 2;
+    A_half(round(f_cut * length(A_half)/ fs_acc):end) = 0;
+
+    if mod(length(A),2)==0
+        A_rec = [A_half; 0; conj(A_half(end:-1:2))];
+    elseif mod(length(A),2)~=0
+        A_rec = [A_half; conj(A_half(end:-1:2))];
+    end
+ 
+    a_cleaned = ifft ((A_rec));
+   
+    N = 10;
+    b = ones(1, N) / N;
+    a = 1;
+    a_filt = filtfilt(b, a, acc); %media mobile senza introduzione di ritardo
+    %% derivata velocità
+
+    dt = 1/20; %1 fratto la freq di campionamento
+    dv = gradient(speed, dt); %derivata della velocità
+
+
+    DV = fft(dv);
+
+
+    fs_GPS = 1;
+    N = length(abs(DV));
+    f_GPS = (0:N-1)*(fs_GPS/N);
+    
+    %% richiamo energy
+   E_no_regen(i) = Energy (a_filt, speed, time_GPS, time_acc);
 end
-A_half = A(1:half_len);
-f_cut = 2;
-A_half(round(f_cut * length(A_half)/ fs_acc):end) = 0;
-
-if mod(length(A),2)==0
-    A_rec = [A_half; 0; conj(A_half(end:-1:2))];
-elseif mod(length(A),2)~=0
-    A_rec = [A_half; conj(A_half(end:-1:2))];
-end
-
-a_cleaned = ifft ((A_rec));
-a_mmean = movmean(acc, 10);
-%% derivata velocità
-
-dt = 1/20; %1 fratto la freq di campionamento
-dv = gradient(speed, dt); %derivata della velocità
-
-
-DV = fft(dv);
-
-
-fs_GPS = 1;
-N = length(abs(DV));
-f_GPS = (0:N-1)*(fs_GPS/N);
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- Script/Energy.m
+++ Script/Energy.m
@@ -1,20 +1,26 @@
-a = a_cleaned;
+function [E] = Energy (acc_cleaned, velocity, time_gps, time_acc)
+%time_gps = Position.Timestamp; --> dati presi dal gps
+%time_acc = Acceleration.Timestamp; --> dati presi dall'accelerometro
+
+
+a = acc_cleaned;
+% a_pos = a; 
+% a_pos(a_pos < 0) = 0; %tolgo gli elementi negativi dell'acc per trovare la potenza senza rigenerazione
 m = 20e3;
-v = speed;
-t_gps = Position.Timestamp;
-t_acc = Acceleration.Timestamp;
-t_s = seconds(t_acc - t_acc(1));
+v = velocity;
+
+t_s = seconds(time_acc - time_acc(1));
 g = 9.80665; % accelerazione di gravità
 Crr = 0.008; % coefficiente di attrito ruota strada
 rho = 1.225; % coefficiente attrito dell'aria
 CdA = 8; % area frontale bus per attrito aero
 eta = 0.8; % efficienza trasmissione tra batteria e ruote
 
-v_interp = interp1(t_gps, v, t_acc, 'linear', 'extrap');
+v_interp = interp1(time_gps, v, time_acc, 'linear', 'extrap');
 % figure
-% plot(t_gps, v);
+% plot(time_gps, v);
 figure
-plot(t_acc, v_interp*3.6);
+plot(time_acc, v_interp*3.6);
 grid on
 grid minor
 title('velocità interpolata km/h')
@@ -26,10 +32,10 @@ p_aux = 5000;
 p_acc = m *a .*v_interp;
 
 figure
-plot(t_acc , p_acc);
+plot(time_acc , p_acc);
 grid on
 grid minor
-title('potenza istantanea')
+title('potenza istantanea (solo m*a*v)')
 
 %% calcolo potenza attrito ruota gomma
 p_roll = m * g * Crr .* v_interp;
@@ -37,21 +43,21 @@ p_roll = m * g * Crr .* v_interp;
 %% calcolo potenza attrito aerodinamico
 p_aero   = 0.5 * rho * CdA .* v_interp.^3;
 
-%% calcolo energia
-p_acc_no_regen = p_acc;
-for k=1:1:length(p_acc)
-    if p_acc(k)<0
-        p_acc_no_regen(k)=0;
-    end
-end
-
+%% calcolo energia senza considerare la frenata rigenerativa
+p_mov_no_regen = p_acc + p_roll + p_aero;
+p_mov_no_regen (p_mov_no_regen<0) = 0;  %per spiegazioni vedi appunti
 % figure
-% plot(t_acc, p_acc_no_regen)
+% plot(time_acc, p_acc_no_regen)
 
-p_tot_raw = p_acc_no_regen + p_roll + p_aero + p_aux;
-p_battery = p_tot_raw./eta;
+p_tot_no_regen = (p_mov_no_regen + p_aux)./eta; %assumendo la stessa efficienza anche per il passaggio batt --> aux
 
-E = trapz(t_s, p_battery)/ 3.6e6;
+figure
+plot(time_acc , p_tot_no_regen);
+grid on
+grid minor
+title('potenza istantanea batteria no regen')
+
+E = trapz(t_s, p_tot_no_regen)/3.6e6;
 % disp(E_no_regen);
 % 
 % E_roll = trapz(t_s, p_roll)/ 3.6e6;
@@ -61,4 +67,6 @@ E = trapz(t_s, p_battery)/ 3.6e6;
 % E_aux = trapz(t_s, p_aux)/ 3.6e6;
 % 
 % E = E_no_regen + E_roll + E_aero + E_aux;
-disp(E);  % in kWh !!
+% disp(E);  % in kWh !!
+
+end
